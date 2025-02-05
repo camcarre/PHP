91640589c32c758b5216efb952ecfdee6807cc9d
theo.delporte <theo.delporte@ynov.com>
2025-02-05 15:53:01 +0100

    ajout migration pour bdd

--- LARAVEL/app/Models/Superhero.php
+++ LARAVEL/app/Models/Superhero.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Superhero extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'real_name',
+        'pseudo',
+        'gender',
+        'planet_origin',
+        'description',
+        'superpowers',
+        'protection_city',
+        'gadgets',
+        'team',
+        'vehicle'
+    ];
+
+    protected $casts = [
+        'superpowers' => 'array',
+        'gadgets' => 'array',
+    ];
 }
